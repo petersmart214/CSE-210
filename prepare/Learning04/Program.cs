22297d8b64d444c57034608b958e2c6555bceeb4
peterksmart <peter.kent.smart@gmail.com>
2025-02-20 14:21:40 -0700

    Learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment tmp_m = new MathAssignment("John Jacob", "Math 102", "Section 7.2", "Problems 1-762");
+        WritingAssignment tmp_w = new WritingAssignment("Gerald Greedle", "Writing for Greedles", "The Impact of Greedlization and it's Developments");
+        Console.WriteLine(tmp_m.GetSummary() + "\n");
+        Console.WriteLine(tmp_w.GetSummary());
+    }
+}
+
+
+class Assignment {
+
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic) {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public virtual string GetSummary() {
+        return $"{_studentName} - {_topic}";
+    }
+}
+
+
+class MathAssignment : Assignment {
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic) {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()}\n{_textbookSection}, {_problems}";
+    }
+}
+
+class WritingAssignment : Assignment {
+    private string _title;
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic) {
+        _title = title;
+    }
+
+    public override string GetSummary() {
+        return $"{base.GetSummary()}\n{_title}";
     }
 }
